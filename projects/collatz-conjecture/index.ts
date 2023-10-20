13655b23246e3d2192920fdf11e6e16192e58760
Danny Nisenbaum <dnny.nis@gmail.com>
2023-10-19 21:03:39 -0300

    added simple collatz challenge

---
+++ projects/collatz-conjecture/index.ts
@@ -0,0 +1,10 @@
+function collatz(n: number) {
+  const sequence = [n];
+  while (n !== 1) {
+    n = n % 2 === 0 ? n / 2 : n * 3 + 1;
+    sequence.push(n);
+  }
+  return sequence.join('->');
+}
+
+console.log(collatz(13));
